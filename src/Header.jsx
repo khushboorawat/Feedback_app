12ca6fbc65a3c4d6c8ff4e3c98e0f9f7845ba524
khushboo <40656656+khushboorawat@users.noreply.github.com>
2022-09-11 17:21:24 +0530

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from 'prop-types'
+
+
+function Header({text,bgColor,textColor}) {
+  const headerStyles = { 
+    backgroundColor: bgColor,
+     color:textColor}
+  return (
+    
+    <header style={headerStyles}>
+     <div className="container">
+        <h1>{text}</h1>
+     </div>
+    </header>
+    
+  );
+}
+Header.defaultProps = {
+  text: 'Feedback UI' ,
+  bgColor:'rgba(0,0,0,0.4)',
+  textColor:'pink',
+}
+Header.prototypes ={
+  text : PropTypes.string,
+  textColor :PropTypes.string,
+  bgColor:PropTypes.string,
+}
+
+
+export default Header;
