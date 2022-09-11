491869533a9275ebb4ad0157906f67b622fe4c13
khushboo <40656656+khushboorawat@users.noreply.github.com>
2022-09-11 17:23:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import {useState} from 'react';
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './Data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+
+
+function App() { 
+  const [feedback,setFeedback] =useState(FeedbackData)
+ const deleteFeedback=(id)=>{
+  if(window.confirm('Are you sure you want to delete?')){
+  setFeedback(feedback.filter((item)=>item.id!==id))
+ }}
+
+ const addFeedback = (newFeedback) => {
+  console.log(newFeedback)
+ }
+  return (
+   <>
+   <Header />
+     <div className='container'>
+      <FeedbackForm handleAdd={addFeedback}/>
+      <FeedbackStats feedback={feedback}/>
+      <FeedbackList feedback={feedback}
+      handleDelete={deleteFeedback} />
+    </div>
+    </>
+
+    
+  );
+}
+
+
+export default App;
